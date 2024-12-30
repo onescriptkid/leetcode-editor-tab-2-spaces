c373acef4c4c3d51d3fdb987ba66e092e8e38038
onescriptkid <onescriptkid@gmail.com>
2024-12-29 19:37:48 -0800

    barely functional solution

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# leetcode-editor-tab-2-spaces
-📝Set leetcode editor to 2 spaces
+# 🪄 leetcode-editor-tab-2-spaces
+
+The leetcode editor persists a **4 space tab** even when saved & set to **2 spaces** - and this bug collects dust on leetcode's backlog - [1](https://github.com/LeetCode-Feedback/LeetCode-Feedback/issues/9314), [2](https://leetcode.com/discuss/feedback/1204698/leetcode-editor-spaces), [3](https://leetcode.com/discuss/general-discussion/443045/leetcode-text-editor-minor-issue-that-bugs-me).
+
+![Automatic](assets/animation.gif)
+
+The attached code snippet, `leetcode-editor-tab-2-spaces.js`, automatically forces a **2 space tab**
+The script's brittle, jank, and contingent on leetcode not changing their dom substantially.
+
+## ✂️ Copy & Paste the snippet into the console to automatically
+
+`leetcode-editor-tab-2-spaces.js`
+```js
+async function toggleLeetCodeEditor() {
+	//...
+}
+toggleLeetCodeEditor()
+```
+
+## 🧩 WIP extension or automatic solution
+Copy & pasting the script into the console is marginally better than clicking through the modal manually.
+Creating an extension and automatically running on domload is ideal.
+
---
+++ leetcode-editor-tab-2-spaces.js
@@ -0,0 +1,80 @@
+// Toggle Leetcode editor spaces
+async function toggleLeetCodeEditor() {
+
+  // Wait for dom elements to load (error prone / jank)
+  const wait = () => {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        res()
+      }, 100)
+    })
+  }
+
+
+  // Click settings icon
+  await wait()
+  const settingsButton = document.getElementById('nav-setting-btn')
+  if (!settingsButton) console.error('Unable to click settings button')
+  settingsButton?.click()
+
+  // Click "Code Editor" tab button
+  await wait()
+  const tabs = document.querySelectorAll('button.relative.inline-flex.items-center.text-sm.font-medium')
+  const codeEditorTab = tabs?.[1]
+  if (!codeEditorTab) console.error('Unable to click codeEditorTab')
+  codeEditorTab?.click()
+
+  // Click tab size toggle
+  await wait()
+  let mostButtons = document.querySelectorAll('button.flex.cursor-pointer.items-center.rounded.text-left')
+  let tabSizeToggle = undefined
+  for (let i = 0; i < mostButtons.length; i++) {
+    const button = mostButtons[i]
+    if (button.textContent === '2 spaces' || button.textContent === '4 spaces') {
+      tabSizeToggle = button
+      break;
+    }
+  }
+  if (!tabSizeToggle) console.error('Unable to click tabSize toggle')
+  tabSizeToggle?.click()
+
+  // Click tab size 4 spaces option
+  await wait()
+  let options = document.querySelectorAll('li.relative > div > div')
+  const tabSizeOption4Spaces = options?.[0]
+  if (!tabSizeOption4Spaces) console.error('Unable to click 4 spaces option')
+  tabSizeOption4Spaces?.click()
+
+  // Reopen tab size toggle
+  await wait()
+  mostButtons = document.querySelectorAll('button.flex.cursor-pointer.items-center.rounded.text-left')
+  tabSizeToggle = undefined
+  for (let i = 0; i < mostButtons.length; i++) {
+    const button = mostButtons[i]
+    if (button.textContent === '2 spaces' || button.textContent === '4 spaces') {
+      tabSizeToggle = button
+      break;
+    }
+  }
+  if (!tabSizeToggle) console.error('Unable to reopen tabSize toggle')
+  tabSizeToggle?.click()
+
+  // Click tab size 2 spaces option
+  await wait()
+  options = document.querySelectorAll('li.relative > div > div')
+  const tabSizeOption2Spaces = options?.[1]
+  if (!tabSizeOption2Spaces) console.error('Unable to click 2 spaces option')
+  tabSizeOption2Spaces?.click()
+
+  // Close modal
+  await wait()
+  const closeButton = document.querySelector('.ring-offset-sd-background')
+  if (!closeButton) console.error('Unable to click closeButton')
+  closeButton?.click()
+
+  // Success
+  console.log('Toggled editor to 2 spaces')
+
+}
+
+toggleLeetCodeEditor()
